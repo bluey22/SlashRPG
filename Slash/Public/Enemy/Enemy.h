928e8cb7c927a4379e75d3f981ed52b224b648bd
Ben Luo <156853404+bluey22@users.noreply.github.com>
2024-03-14 00:33:06 -0400

    March 14th: Refactoring for Combat
    Refactoring inheritance of Character and Enemy classes to prepare for combat implementation.
    Slowly but surely :)

---
+++ Slash/Public/Enemy/Enemy.h
@@ -0,0 +1,90 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/BaseCharacter.h"
+#include "Characters/CharacterTypes.h"
+#include "Enemy.generated.h"
+
+class UHealthBarComponent;
+class UPawnSensingComponent;
+
+UCLASS()
+class SLASH_API AEnemy : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEnemy();
+
+	virtual void Tick(float DeltaTime) override;
+	void CheckPatrolTarget();
+	void CheckCombatTarget();
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UHealthBarComponent* HealthBarWidget;
+
+	UPROPERTY(VisibleAnywhere)
+	UPawnSensingComponent* PawnSensing;
+
+	/**
+	*Animation Montages
+	*/
+
+	UPROPERTY()
+	AActor* CombatTarget;
+
+	UPROPERTY(EditAnywhere)
+	double CombatRadius = 650.f;
+
+	UPROPERTY(EditAnywhere)
+	double AttackRadius = 150.f;
+
+	/**
+	* Navigation
+	*/
+
+	UPROPERTY()
+	class AAIController* EnemyController;
+
+	//Current Patrol Target
+	UPROPERTY(EditInstanceOnly, Category = "AI Navigation")
+	AActor* PatrolTarget;
+
+	UPROPERTY(EditInstanceOnly, Category = "AI Navigation")
+	TArray<AActor*> PatrolTargets;
+
+	UPROPERTY(EditAnywhere)
+	double PatrolRadius = 200.f;
+
+	FTimerHandle PatrolTimer;
+	void PatrolTimerFinished();
+
+	UPROPERTY(EditAnywhere, Category = "AI Navigation")
+	float WaitMin = 3.f;
+
+	UPROPERTY(EditAnywhere, Category = "AI Navigation")
+	float WaitMax = 10.f;
+
+	EEnemyState EnemyState = EEnemyState::EES_Patrolling;
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Die() override;
+	bool InTargetRange(AActor* Target, double Radius);
+	void MoveToTarget(AActor* Target);
+	AActor* ChoosePatrolTarget();
+
+	UFUNCTION() // bc bound to delegate
+	void PawnSeen(APawn* SeenPawn);
+
+	UPROPERTY(BlueprintReadOnly)
+	EDeathPose DeathPose = EDeathPose::EDP_Alive;
+
+};
